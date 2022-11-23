12096689a65ed8f5a85444f3f90131e43e0387a6
Matrix <rhailu88@gmail.com>
2022-11-22 22:17:57 -0500

    Gets forecast for the next five days in script.js.

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -74,3 +74,68 @@ var latValue = weatherData.coord.lat;
 var lonValue = weatherData.coord.lon;
 var queryUv = "https://api.openweathermap.org/data/2.5/uvi?";
 var uvdata = queryUv + "lat=" + latValue + "&lon=" + lonValue + "&appid=" + apiKey
+
+
+$.ajax({
+    url: uvdata,
+    method: "GET",
+  }).then(function (uvIndexData) {
+    var uvIndex = document.getElementById("uv-index");
+    uvIndex.textContent = "UV Index: " + uvIndexData.value;
+
+    var uvText = uvIndexData.value;
+    if (uvText <= 2) {
+      uvIndex.setAttribute("class", "badge bg-success");
+    }
+    else if (uvText <= 6) {
+      uvIndex.setAttribute("class", "badge bg-warning");
+    }
+    else if (uvText > 6) {
+      uvIndex.setAttribute("class", "badge bg-danger");
+    }
+  });
+
+  //gets forecast data and converts it to the next 5 days. creates weather cards for the next 5 days
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/onecall?units=imperial&" + "lat=" + latValue + "&lon=" + lonValue + "&exclude=current,minutely,hourly,alerts" + "&appid=" + apiKey,
+    method: "GET",
+  }).then(function (forecastData) {
+    $("#forecast").empty();
+
+    for (var i = 1; i < 6; i++) {
+      var forecastSection = document.getElementById("forecast");
+
+      var unix_timestamp = forecastData.daily[i].dt;
+      var date = new Date(unix_timestamp * 1000);
+      var forecastDate = dayjs(date).format('MM/DD/YYYY');
+
+      var div1 = document.createElement("div");
+      div1.setAttribute("class", "col-sm");
+      forecastSection.appendChild(div1);
+
+      var div2 = document.createElement("div");
+      div2.setAttribute("class", "card card-body bg-primary border-dark");
+      div1.appendChild(div2);
+
+      var ptag1 = document.createElement("p");
+      ptag1.textContent = forecastDate;
+      div2.appendChild(ptag1);
+
+      var img2 = document.createElement('img');
+      img2.setAttribute("src", "https://openweathermap.org/img/wn/" + forecastData.daily[i].weather[0].icon + "@2x.png");
+      img2.setAttribute("alt", forecastData.daily[i].weather[0].description);
+      div2.appendChild(img2);
+
+      var forecastTemp = forecastData.daily[i].temp.day;
+      var ptag2 = document.createElement("p");
+      div2.appendChild(ptag2);
+      ptag2.textContent = "Temp:" + forecastTemp + "°F";
+
+      var forecastHumidity = forecastData.daily[i].humidity;
+      var ptag3 = document.createElement("p");
+      div2.appendChild(ptag3);
+      ptag3.textContent = "Humidity:" + forecastHumidity + "%";
+    }
+  })
+});
+};
